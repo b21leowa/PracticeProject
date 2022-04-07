d9837cd977221f20dbfd63163d6490133f34a631
Leo Wahlandt <b21leowa@student.his.se>
2022-04-07 09:46:14 +0200

    Added List and BergsAdapter

--- app/src/main/java/com/example/practiceproject/MainActivity.java
+++ app/src/main/java/com/example/practiceproject/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.practiceproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> berg = Arrays.asList("Mount Everest", "Bilingen", "K2", "Kebnekaise", "Hanneberg", "Hunneberg");
+
+        recyclerView = findViewById(R.id.recyclerView);
     }
 }
