cdb61db78e74a1618bb719b10b8b0b4e5b101d39
Leo Wahlandt <b21leowa@student.his.se>
2022-04-07 10:45:02 +0200

    Implemented BergsAdapter

--- app/src/main/java/com/example/practiceproject/BergsAdapter.java
+++ app/src/main/java/com/example/practiceproject/BergsAdapter.java
@@ -1,33 +1,44 @@
 package com.example.practiceproject;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class BergsAdapter extends RecyclerView.Adapter<BergsAdapter.BergsAdapterViewHolder> {
+    private List<String> berg;
+
+    public BergsAdapter(List<String> berg) {
+        this.berg = berg;
+    }
 
     @NonNull
     @Override
     public BergsAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new BergsAdapterViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull BergsAdapterViewHolder holder, int position) {
-
+        holder.bergsNamn.setText(berg.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.berg.size();
     }
 
     public class BergsAdapterViewHolder extends RecyclerView.ViewHolder {
-
+        private TextView bergsNamn;
         public BergsAdapterViewHolder(@NonNull View itemView) {
             super(itemView);
+            bergsNamn = itemView.findViewById(R.id.bergs_namn);
         }
     }
 }
