e4700c25bf2fb5bb6aed8af173a6c6720070e49e
Leo Wahlandt <b21leowa@student.his.se>
2022-04-07 09:56:43 +0200

    Added template code for bergsAdapter

--- app/src/main/java/com/example/practiceproject/BergsAdapter.java
+++ app/src/main/java/com/example/practiceproject/BergsAdapter.java
@@ -1,4 +1,33 @@
 package com.example.practiceproject;
 
-public class BergsAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BergsAdapter extends RecyclerView.Adapter<BergsAdapter.BergsAdapterViewHolder> {
+
+    @NonNull
+    @Override
+    public BergsAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BergsAdapterViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class BergsAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        public BergsAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
